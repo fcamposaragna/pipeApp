7ffe09699bb602a93155823ce8ffb87cc3bc6cc3
Franco Camposaragna <fcamposaragna@gmail.com>
2022-10-28 16:32:13 -0300

    App terminada

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interfaces';
 
 @Component({
   selector: 'app-ordenar',
@@ -8,9 +9,37 @@ import { Component } from '@angular/core';
 export class OrdenarComponent  {
 
   enMayuscula:boolean = true;
+  ordenarPor:string = '';
+
+  heroes: Heroe[] = [
+    {
+      nombre: 'Superman',
+      vuela: true,
+      color: Color.azul
+    },
+    {
+      nombre: 'Batman',
+      vuela: false,
+      color: Color.negro
+    },
+    {
+      nombre: 'Robin',
+      vuela: false,
+      color: Color.verde
+    },
+    {
+      nombre: 'Thor',
+      vuela: true,
+      color: Color.rojo
+    }
+  ];
 
   cambiar(){
     this.enMayuscula = !this.enMayuscula;
+  }
+
+  cambiarOrden(valor:string){
+    this.ordenarPor = valor;
   }
 
 }
